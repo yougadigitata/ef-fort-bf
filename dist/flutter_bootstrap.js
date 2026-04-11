0fd63c53ccba6225d8a0599e1c04b3b898a667f0
EF-FORT Dev <dev@ef-fort-bf.com>
2026-04-11 11:31:46 +0000

    Build Flutter web v6.0 — dist/ mis à jour (19e matière Haut niveau)

--- dist/flutter_bootstrap.js
+++ dist/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3423937699"
+    serviceWorkerVersion: "4178909960"
   }
 });
